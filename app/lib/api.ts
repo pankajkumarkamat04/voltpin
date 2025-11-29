064a9e4fafe7fd28cf5adf2143d66a4c5a0397a1
pankajkumarkamat04 <pankajkumarkamat04@gmail.com>
2025-11-29 15:38:15 +0530

    Updated API Base URL

--- app/lib/api.ts
+++ app/lib/api.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'https://api.credszone.com/api/v1';
+const API_BASE_URL = 'https://api.vlotpin.in/api/v1';
 
 // Helper function to get auth token
 const getAuthToken = (): string | null => {
